b7eb691e1151c7294c7c8b695523cdf4041935c1
varshithakarthik <82289117+varshithakarthik@users.noreply.github.com>
2021-11-07 19:08:52 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -24,23 +24,24 @@ function setup() {
 	helicopterSprite.addImage(helicopterIMG)
 	helicopterSprite.scale=0.6
 
-	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite=createSprite(width/2, height-15, width,10);
 	groundSprite.shapeColor=color(255)
-
 
 
 	engine = Engine.create();
 	world = engine.world;
-
-	packageBody = Bodies.circle(width/2 , 200 , 5, {restitution:0.8});
-	Matter.Body.setStatic(packageBody, true);
+	side1= new Ground(500,670,200,20);
+	side2= new Ground(400,620,20,100);
+	side3= new Ground(600,620,20,100);
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.3, isStatic:true});
 	World.add(world, packageBody);
 	
 
 	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
-	 World.add(world, ground);
-	
+ 	World.add(world, ground);
+
+
 	Engine.run(engine);
   
 }
@@ -49,21 +50,28 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
-  
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
   drawSprites();
-  keyPressed();
- 
+  side1.display();
+  side2.display();
+  side3.display();
  
 }
 
 function keyPressed() {
- if (keyCode == DOWN_ARROW) {
-	 console.log("down")
-	Matter.Body.setStatic(packageBody, false);
-	   
-	
+	if (keyCode === RIGHT_ARROW) {
+		helicopterSprite.x+=20;
+		Matter.Body.translate(packageBody,{x:20,y:0});
+	}
+	if (keyCode === LEFT_ARROW) {
+		helicopterSprite.x-=20;
+		Matter.Body.translate(packageBody,{x:-20,y:0});
+	}
+
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on
+    Matter.Body.setStatic(packageBody,false);
   }
 }
 
