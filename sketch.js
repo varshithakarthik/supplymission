2fadfcbdeb4cd10c8d5819199c2d32a7152fb673
varshithakarthik <82289117+varshithakarthik@users.noreply.github.com>
2021-11-07 19:18:25 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
-var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground
+var helicopterIMG, helicopterSprite, packageSprite, packageIMG;
+var packageBody;
+var box1,box2,box3;
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -16,7 +18,7 @@ function setup() {
 	rectMode(CENTER);
 	
 
-	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite=createSprite(width/2, 80, 20,20);
 	packageSprite.addImage(packageIMG)
 	packageSprite.scale=0.2
 
@@ -24,26 +26,26 @@ function setup() {
 	helicopterSprite.addImage(helicopterIMG)
 	helicopterSprite.scale=0.6
 
-	groundSprite=createSprite(width/2, height-15, width,10);
-	groundSprite.shapeColor=color(255)
+	//groundSprite=createSprite(width/2, height-35, width,10);
+	//groundSprite.shapeColor=color(255)
 
 
 	engine = Engine.create();
 	world = engine.world;
-	side1= new Ground(500,670,200,20);
-	side2= new Ground(400,620,20,100);
-	side3= new Ground(600,620,20,100);
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.3, isStatic:true});
+
+	packageBody = Bodies.circle(width/2 , 200 , 20 , {restitution:0.2, isStatic:true});
 	World.add(world, packageBody);
-	
+
 
 	//Create a Ground
-	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
- 	World.add(world, ground);
+	//ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	//World.add(world, ground);
 
-
+	box2 = new Box(500,650,20,100,"red");
+	box3 = new Box(295,650,20,100,"red");
+	box1 = new Box(400,690,200,20,"red");
+	
 	Engine.run(engine);
-  
 }
 
 
@@ -53,25 +55,15 @@ function draw() {
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
   drawSprites();
-  side1.display();
-  side2.display();
-  side3.display();
- 
+
+	
+  if(keyDown(DOWN_ARROW)) {
+	Matter.Body.setStatic(packageBody,false);
+	}	
+	
+	box1.display();
+	box2.display();
+	box3.display();
+
+  Engine.update(engine);
 }
-
-function keyPressed() {
-	if (keyCode === RIGHT_ARROW) {
-		helicopterSprite.x+=20;
-		Matter.Body.translate(packageBody,{x:20,y:0});
-	}
-	if (keyCode === LEFT_ARROW) {
-		helicopterSprite.x-=20;
-		Matter.Body.translate(packageBody,{x:-20,y:0});
-	}
-
- if (keyCode === DOWN_ARROW) {
-    // Look at the hints in the document and understand how to make the package body fall only on
-    Matter.Body.setStatic(packageBody,false);
-  }
-}
-
